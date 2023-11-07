1994b1581b46ae59cc7a3a9832b2239c4039d899
Vance-ng-vn <vance.ng.vn@gmail.com>
2023-11-07 12:26:48 +0700

    Auto-commit

--- domain-list.js
+++ domain-list.js
@@ -1,5 +1,7 @@
 const external_domains = [
-    "https://subscene.onrender.com",
-    "https://subscene-ip8j.onrender.com"
+    "https://subscene.onrender.com", //#zbm5rljs us 61120
+    "https://subscene-ip8j.onrender.com", //asia 61121
+    "https://subscene-h090.onrender.com", //#ob0b034u us 61122
+    "https://subscene-ljxx.onrender.com" //eu 61123
 ]
 module.exports = external_domains;
