55f01d678bf003c5b5b30a8f755e974f44469d84
Vance-ng-vn <vance.ng.vn@gmail.com>
2023-11-11 11:19:49 +0700

    Auto-commit

--- modules/logger.js
+++ modules/logger.js
@@ -37,9 +37,11 @@ const logger = {
         if(fs.statSync(file).size/1024 >= 256) this.emptyError();
         if(message?.stack) {
             cons.log(message.stack);
+            this.log(message.stack);
             fs.appendFileSync(file, message.stack + '\n\n');
         } else {
             cons.log(message?.toString());
+            this.log(message?.toString());
             fs.appendFileSync(file, message?.toString() + '\n');
         }
 
